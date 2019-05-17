81dbcc3f6c81bde52f95c1e5d7e86ac90a7d606e
Sean1710 <50674878+Sean1710@users.noreply.github.com>
2019-05-17 14:05:46 -0400

    Add files via upload

---
+++ knightsofcarnage.java
@@ -0,0 +1,76 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import javax.swing.*;
+import javax.imageio.*;
+import java.io.*;
+
+public class knightsofcarnage{
+
+	//Properties
+	JFrame theframe;
+	kofcPanel thepanel;
+	Timer thetimer;
+	JLabel thelabel;
+
+		
+	
+	//Methods
+	
+		
+	public void mouseMoved(MouseEvent evt){
+		
+	}
+	
+	public void mouseDragged(MouseEvent evt){
+
+	}
+	
+	public void keyTyped(KeyEvent evt){
+		
+	}
+	
+	public void keyReleased(KeyEvent evt){
+
+	}
+	
+	public void mouseEntered(MouseEvent evt){
+		
+	}
+	
+	public void keyPressed(KeyEvent evt){
+
+	}
+
+	public void mouseReleased(MouseEvent evt){
+		
+	}
+	public void mousePressed(MouseEvent evt){
+		
+	}
+	public void mouseClicked(MouseEvent evt){
+		
+	}
+	
+
+
+	//Constructor
+
+
+	public knightsofcarnage(){
+		
+	}
+	
+	
+	// main method
+	public static void main(String[] args){
+		new knightsofcarnage();
+	}
+}
+
+
+
+
+
+
+
---
+++ kofcPanel.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import javax.swing.*;
+import javax.imageio.*;
+import java.io.*;
+
+public class kofcPanel extends JPanel{
+	
+	//Properties
+	
+	
+	
+	
+	//Methods
+	
+	public void paintComponent(Graphics g){
+		
+		
+	}
+		
+		
+	//Constructor
+
+	public kofcPanel(){
+			super();
+		}
+	}
+
+
+
+
+
