8ecc4793366690617f303fbb3f3b94e04299e92e
Sean1710 <50674878+Sean1710@users.noreply.github.com>
2019-06-03 14:28:58 -0400

    Add files via upload

--- kofc.java
+++ kofc.java
@@ -11,6 +11,7 @@ public class kofc implements ActionListener, MouseMotionListener, KeyListener, M
 	
 	JFrame theframe;
 	kofcPanel thepanel;
+	SuperSocketMaster ssm;
 	Timer thetimer;
 	Timer thetimer2;
 	JButton Play;
@@ -19,8 +20,9 @@ public class kofc implements ActionListener, MouseMotionListener, KeyListener, M
 	JButton Back1;
 	JButton Back2;
 	JButton Back3;
+	JButton Confirm;
 	JButton Client;
-	JButton Server;
+	JButton Host;
 	JButton ChooseRed;
 	JButton ChooseBlue;
 	JButton ChooseMage;
@@ -30,55 +32,141 @@ public class kofc implements ActionListener, MouseMotionListener, KeyListener, M
 	JButton Attack;
 	JButton Block;
 	JButton Heal;
+	JTextField txtusername;
 	int intHealth1; 
 	int intHealth2;
 	int intHealth3;
 	int intHealth4;
-	String strName1;
-	String strName2;
-	String strName3;
-	String strName4;
 	int intNum = 0;
 	int intMenu = 1;
+	String strusername;
+	String strIP;
+	boolean blnHost;
+	boolean blnClient;
 		
 	
 	//Methods
 
 	public void actionPerformed(ActionEvent evt){
-		if(evt.getSource() == this.Play){
-			
-			Play.setVisible(false);
-			
-			Back1 = new JButton("Back");
-			Back1.setSize(200, 200);
-			Back1.setLocation(0, 0);
-			thepanel.add(Back1);
-			Back1.setOpaque(false);
-			Back1.setContentAreaFilled(false);
-			Back1.setBorderPainted(false);
-			Back1.setVisible(true);
-	
-		}
 		
-		if(evt.getSource() == this.Help){		
-			thepanel.blnMenu = false;
-			
-
-		}
-		
-		if(evt.getSource() == this.Settings){		
-			
-		}
-	
-		if(evt.getSource()==thetimer){
-			thepanel.repaint();
-		}
-		
-		if(evt.getSource() == this.Back1){
-			System.out.println("pressed");
-			Back1.setVisible(false);
+		//Main menu, unhide play help and settings buttons
+		if(intMenu == 1){
 			Play.setVisible(true);
+			Help.setVisible(true);
+			Settings.setVisible(true);
+		
+			if(evt.getSource() == this.Play){
+				Play.setVisible(false);
+				Help.setVisible(false);
+				Settings.setVisible(false);
+				intMenu = 4;
+				
+			}
+			
+			if(evt.getSource() == this.Help){		
+				Play.setVisible(false);
+				Help.setVisible(false);
+				Settings.setVisible(false);
+				intMenu = 2;
+
+			}
+			
+			if(evt.getSource() == this.Settings){		
+				Play.setVisible(false);
+				Help.setVisible(false);
+				Settings.setVisible(false);
+				intMenu = 3;
+			}
+		
+			if(evt.getSource()==thetimer){
+				thepanel.repaint();
+			}
+			
+		//Help Screen, unhide back1 button
+		}else if(intMenu == 2){
+			Back1.setVisible(true);
+			if(evt.getSource() == this.Back1){
+				intMenu = 1;
+				Back1.setVisible(false);
+			}
+			
+			
+		//Settings Screen, unhide back2 button
+		}else if(intMenu == 3){
+			Back2.setVisible(true);
+			if(evt.getSource() == this.Back2){
+				intMenu = 1;
+				Back2.setVisible(false);			
+			}
+		
+		//Username Screen, unhide back3 button
+		}else if(intMenu == 4){
+			Back3.setVisible(true);
+			Confirm.setVisible(true);
+			txtusername.setVisible(true);
+			if(evt.getSource() == this.Back3){
+				intMenu = 1;
+				Back3.setVisible(false);
+				Confirm.setVisible(false);
+				txtusername.setVisible(false);
+			}else if(evt.getSource() == this.Confirm){
+				intMenu = 5;
+				txtusername.setVisible(false);
+				Confirm.setVisible(false);
+				Back3.setVisible(false);
+				strusername = txtusername.getText();
+				System.out.println("Username: " + strusername);
+			}	
+	
+		//Client/Host choice Screen, unhide client and server buttons
+		}else if(intMenu == 5){
+			Host.setVisible(true);
+			Confirm.setVisible(true);
+			//Host
+			if (evt.getSource() == Host) {
+				System.out.println("Multiplayer: Host");
+				blnHost = true;
+				Host.setVisible(false); 
+				Client.setVisible(false);
+				ssm = new SuperSocketMaster(1337, this); 
+				System.out.println(ssm.getMyAddress());
+				strIP = ssm.getMyAddress(); // Convert IP Address into String strIP
+				ssm.connect();
+				intMenu = 6; // Change to HostMenu
+			}
+			//Client
+			else if (evt.getSource() == Client) {
+				System.out.println("Multiplayer: Guest");
+				blnClient = true;
+				Host.setVisible(false); 
+				Client.setVisible(false);
+				intMenu = 7; // Change to GuestMenu
+			}
+			
+			
+		//Host Screen
+		}else if(intMenu == 6){	
+		
+		
+		//Client Screen
+		}else if(intMenu == 7){
+		
+		
+		//Choose Team Screen
+		}else if(intMenu == 8){
+			
+			
+		
+		//Character Selection Screen
+		}else if(intMenu == 9){
+		
+		
+		//Gameplay Screen
+		}else if(intMenu == 10){
+			
+		
 		}
+		
 	}
 	public void keyTyped(KeyEvent evt){
 		
@@ -122,22 +210,29 @@ public class kofc implements ActionListener, MouseMotionListener, KeyListener, M
 
 
 
-	//Constructor
-
+	//Constructors
 
 	public kofc(){
-		//Screen
+		
+		//Panel
 		thepanel = new kofcPanel();
 		thepanel.setLayout(null);
 		thepanel.setPreferredSize(new Dimension(1280, 720));
 		thepanel.addMouseMotionListener(this);
+		thepanel.addMouseListener(this);
 		
+		//Frame
 		theframe = new JFrame("Knights of Carnage");
 		theframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		theframe.setContentPane(thepanel);
 		theframe.pack();
 		theframe.setVisible(true);
 		theframe.setResizable(false);
+		theframe.addKeyListener(this);
+		theframe.addMouseListener(this);
+
+		
+		//Timer
 		thetimer = new Timer(1000/60, this);
 		thetimer.start();
 		
@@ -149,8 +244,11 @@ public class kofc implements ActionListener, MouseMotionListener, KeyListener, M
 		Play.setOpaque(false);
 		Play.setContentAreaFilled(false);
 		Play.setBorderPainted(false);
-		Play.setVisible(true);
+		Play.setVisible(false);
+		Play.addActionListener(this);
+	
 		
+		//Help Button
 		Help = new JButton("Help");
 		Help.setSize(150, 50);
 		Help.setLocation(565, 300);
@@ -158,13 +256,97 @@ public class kofc implements ActionListener, MouseMotionListener, KeyListener, M
 		Help.setOpaque(false);
 		Help.setContentAreaFilled(false);
 		Help.setBorderPainted(false);
-		Help.setVisible(true);
-
-		theframe.addKeyListener(this);
-		thepanel.addMouseListener(this);
-		Play.addActionListener(this);
+		Help.setVisible(false);
 		Help.addActionListener(this);
 
+		//Settings
+		Settings = new JButton("Settings");
+		Settings.setSize(150, 50);
+		Settings.setLocation(565, 450);
+		thepanel.add(Settings);
+		Settings.setOpaque(false);
+		Settings.setContentAreaFilled(false);
+		Settings.setBorderPainted(false);
+		Settings.setVisible(false);
+		Settings.addActionListener(this);
+		
+		//Confirm Username
+		Confirm = new JButton("Confirm");
+		Confirm.setSize(150, 50);
+		Confirm.setLocation(565, 300);
+		thepanel.add(Confirm);
+		Confirm.setOpaque(false);
+		Confirm.setContentAreaFilled(false);
+		Confirm.setBorderPainted(false);
+		Confirm.setVisible(false);
+		Confirm.addActionListener(this);
+		
+		//Back from help screen
+		Back1 = new JButton("Back");
+		Back1.setSize(200, 200);
+		Back1.setLocation(0, 0);
+		thepanel.add(Back1);
+		Back1.setOpaque(false);
+		Back1.setContentAreaFilled(false);
+		Back1.setBorderPainted(false);
+		Back1.setVisible(false);
+		Back1.addActionListener(this);
+		
+		//Back from settings screen
+		Back2 = new JButton("Back");
+		Back2.setSize(200, 200);
+		Back2.setLocation(0, 0);
+		thepanel.add(Back2);
+		Back2.setOpaque(false);
+		Back2.setContentAreaFilled(false);
+		Back2.setBorderPainted(false);
+		Back2.setVisible(false);
+		Back2.addActionListener(this);
+
+		
+		// Back from username screen
+		Back3 = new JButton("Back");
+		Back3.setSize(200, 200);
+		Back3.setLocation(0, 0);
+		thepanel.add(Back3);
+		Back3.setOpaque(false);
+		Back3.setContentAreaFilled(false);
+		Back3.setBorderPainted(false);
+		Back3.setVisible(false);
+		Back3.addActionListener(this);
+
+		//Textfield for username
+		txtusername = new JTextField();
+		txtusername.setOpaque(false); 
+		txtusername.setSize(400, 35);
+		txtusername.setLocation(440, 100);
+		txtusername.addActionListener(this);
+		thepanel.add(txtusername);
+		txtusername.setVisible(false); 
+		
+		//Choose Host
+		Host = new JButton("Host");
+		Host.setSize(200, 200);
+		Host.setLocation(400, 300);
+		thepanel.add(Host);
+		Host.setOpaque(false);
+		Host.setContentAreaFilled(false);
+		Host.setBorderPainted(false);
+		Host.setVisible(false);
+		Host.addActionListener(this);
+		
+		//Choose Client
+		Client = new JButton("Client");
+		Client.setSize(200, 200);
+		Client.setLocation(600, 300);
+		thepanel.add(Client);
+		Client.setOpaque(false);
+		Client.setContentAreaFilled(false);
+		Client.setBorderPainted(false);
+		Client.setVisible(false);
+		Client.addActionListener(this);
+		
+		
 		
 	}
 	
--- kofcPanel.java
+++ kofcPanel.java
@@ -9,10 +9,10 @@ public class kofcPanel extends JPanel{
 	
 	//Properties
 	
+	boolean blnMenu; 
 	BufferedImage imgback = null; 
 	BufferedImage imgboard = null;
 	BufferedImage imgmenu = null;
-	boolean blnMenu = true;
 	JButton Play;
 	JPanel thepanel;
 	int intMenu = 1;
@@ -20,28 +20,52 @@ public class kofcPanel extends JPanel{
 	//Methods
 	
 	public void paintComponent(Graphics g){
-		if(blnMenu == true && intMenu == 1){
-			g.drawImage(imgback, 0, 0, null);
 		
-		}else if(blnMenu == true && intMenu == 2){
+		//Main Menu
+		if(blnMenu == true && intMenu == 1){
 			g.drawImage(imgmenu, 0, 0, null);
 		
-		}else if(blnMenu == true && intMenu == 3){
+		//Help
+		}else if(blnMenu == true && intMenu == 2){
 		
-				
+		//Settings
+		}else if(blnMenu == true && intMenu == 3){
+			
+			
+		//Username	
 		}else if(blnMenu == true && intMenu == 4){
 			
+			
+		//Host/Client	
 		}else if(blnMenu == true && intMenu == 5){
 			
-		}else if(blnMenu == true && intMenu == 6){
 			
-		}else if(blnMenu == true && intMenu == 7){
+		//Host	
+		}else if(blnMenu == true && intMenu == 6){
 			g.drawImage(imgboard, 0, 0, null);
-		}	
-		
-		if(blnMenu == false){
-		
+			
+			
+		//Client
+		}else if(blnMenu == true && intMenu == 7){
+			
+			
+			
+		//Choose Team	
+		}else if(blnMenu == true && intMenu == 8){
+			
+			
+			
+		//Character Selection		
+		}else if(blnMenu == true && intMenu == 9){
+			
+			
+			
+		//Gameplay
+		}else if(blnMenu == true && intMenu == 10){
+			
+			
 		}
+		
 		
 }
 	
