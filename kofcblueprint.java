ae89aa0566632d440dc5f85bd061f97b18a14dcb
Sean1710 <50674878+Sean1710@users.noreply.github.com>
2019-06-07 14:18:12 -0400

    Add files via upload

---
+++ kofcblueprint.java
@@ -0,0 +1,45 @@
+public class kofcblueprint{
+
+	//Properties
+	public String strplayerName;
+    public int intplayerhp;
+    public int intmaxhp;
+    public int intmaxmana;
+    public int intmana;
+    public int intplayermeleedmg;
+    public int intenemyhp;
+    public int intenemymeleedmg;
+    public int intLevel;
+    public String strcharclass;
+    //globals for player stats & enemy stats
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
